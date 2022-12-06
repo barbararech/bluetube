d9810f890f4a59c70b331f29b34b17b977606d94
barbararech <rech.brb@gmail.com>
2022-12-06 15:08:29 -0300

    first commit

---
+++ src/middlewares/authentication-middleware.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import * as jwt from 'jsonwebtoken';
+
+import { unauthorizedError } from '@/errors';
+
+export async function authenticateToken(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) return generateUnauthorizedResponse(res);
+
+  const token = authorization?.replace('Bearer ', '').trim();
+  if (!token) return generateUnauthorizedResponse(res);
+
+  try {
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET) as JWTPayload;
+
+    res.locals.userId = userId;
+
+    return next();
+  } catch (err) {
+    return generateUnauthorizedResponse(res);
+  }
+}
+
+function generateUnauthorizedResponse(res: Response) {
+  res.status(httpStatus.UNAUTHORIZED).send(unauthorizedError());
+}
+
+export type AuthenticatedRequest = Request & JWTPayload;
+
+type JWTPayload = {
+  userId: number;
+};
